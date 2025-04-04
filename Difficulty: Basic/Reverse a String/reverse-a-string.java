8d14e0aeecf84b463126c814bfaca21309d2a104
pranavi-reddy503 <23951a04c3@iare.ac.in>
2025-04-04 18:48:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.java
+++ Difficulty: Basic/Reverse a String/reverse-a-string.java
@@ -1,41 +1,37 @@
 //{ Driver Code Starts
-// Initial Template for Java
+// Initial template for Java
 
 import java.io.*;
-import java.lang.*;
 import java.util.*;
 
-class Driver {
+public class GFG {
     public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-
         while (t-- > 0) {
-            String str = read.readLine();
-            System.out.println(new Solution().reverseString(str));
+            String s = read.readLine();
+            Solution ob = new Solution();
 
-            System.out.println("~");
-        }
+            System.out.println(ob.revStr(s));
+        
+System.out.println("~");
+}
     }
 }
 // } Driver Code Ends
 
 
-// User function Template for Java
-
 class Solution {
-    public static String reverseString(String s) {
-        // code here
-        StringBuilder sb=new StringBuilder(s);
+    static String revStr(String s) {
+        StringBuilder sb=new StringBuilder(s); 
         for(int i=0;i<sb.length()/2;i++){
-            int front =i;
+            int front=i;
             int back=sb.length()-1-i;
-            char frontChar=sb.charAt(front);
             char backChar=sb.charAt(back);
+            char frontChar=sb.charAt(front);
             sb.setCharAt(front,backChar);
             sb.setCharAt(back,frontChar);
         }
-    
         return sb.toString();
     }
 }
