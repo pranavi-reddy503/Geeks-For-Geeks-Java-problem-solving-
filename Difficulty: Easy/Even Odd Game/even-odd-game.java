a2932d76a8a4f97bed7e16ffb53c0a97310aee48
pranavi-reddy503 <23951a04c3@iare.ac.in>
2025-05-06 21:01:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+
+    
+// } Driver Code Ends
+
+// User function Template for Java
+
+public static String utility(int n) {
+
+    // write your code here
+    if(n%2==0){
+        return "Friend";
+    }
+    else{
+        return "You";
+    }
+}
+
+
+//{ Driver Code Starts.
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while (t-- > 0) {
+            int number = scn.nextInt();
+            System.out.println(utility(number));
+
+            System.out.println("~");
+        }
+        scn.close();
+    }
+}
+// } Driver Code Ends
